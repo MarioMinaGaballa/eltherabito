a6a2f52133a64af0aa7e5db2b6962e2f4c381546
MarioMinaGaballa <mariomena199@gmail.com>
2026-05-21 12:30:45 +0200

    add Navbar and Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup/Signup';
-import Login  from './pages/Login/Login';
+import Login from './pages/Login/Login';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
 
-const Home      = () => <h1>Home Page</h1>;
 
 export default function App() {
   return (
     <BrowserRouter>
-    
+      <Navbar />
+
       <Routes>
-        <Route path="/"          element={<Home />} />
-        <Route path="/signup"    element={<Signup />} />
-        <Route path="/login"     element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
